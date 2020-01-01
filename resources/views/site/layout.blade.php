458796dc4d0da6a810d6aaadbb8f6b81ea838707
Jon Zenor <JonZenor@gmail.com>
2020-01-01 15:23:42 -0700

    Installed Tailwind and got a very basic ugly page setup. Need to work on it more. #7

---
+++ resources/views/site/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nova+Square|Teko&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <title>@yield('title')</title>
+    </head>
+    <body class="bg-gray-900">
+
+        <div class="bg-gray-100 h-12 font-header">
+            <ul class="flex h-full">
+                <li class="mr-6 bg-black inline-block align-middle">
+                    <a class="text-blue-500 hover:text-blue-800 align-bottom" href="#">Home</a>
+                </li>
+                <li class="mr-6">
+                <a class="text-blue-500 hover:text-blue-800" href="#">Character</a>
+                </li>
+                <li class="mr-6">
+                <a class="text-blue-500 hover:text-blue-800" href="#">Login</a>
+                </li>
+            </ul>
+        </div>
+          
+
+        @yield('body')
+
+    </body>
+</html>
