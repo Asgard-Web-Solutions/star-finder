6bc8188d8be755f634d106a4120b355df2be8a66
Malikai <MalikaiZenor@gmail.com>
2020-01-05 08:46:43 -0700

    closes #4 i got the dadtabase to get a limit of 1 per user and got the spechies  and name and user id working

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -3,17 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Character;
+use App\Species;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CharacterController extends Controller
 {
     public function create () {
 
-        return view('character.new');
+        $user_id = Auth::id();
+        $character = Character::where("user_id", '=', $user_id)->get();
+
+        if ($character->count()) {
+            return redirect('/home');
+        }
+
+        $species = Species::all();
+
+        return view('character.new', [
+            'species' => $species,
+        ]);
     }
 
     public function save (Request $request){
+
         $character = new Character();
+
+        $character->name = $request->name;
+        $character->user_id = Auth::id();
+        $character->species_id = $request->species;
+        $character->faction_id	= 0;
+
+        $character->save();
+
+        return redirect()->route('home');
     }
 
 }
